bdfd58868f563c71cf2b8cc5881b76b20d679778
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-21 16:23:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ def speak_to_grandma (phrase)
   # phrase "HUH?! SPEAK UP, SONNY!"
    if phrase = "I LOVE YOU GRANDMA!"
     return 'I LOVE YOU TOO PUMPKIN!'
+ end
 end
-
 
 
 # Whatever you say to grandma, she should respond with
